28cc855a5ffb10db5eb374fd9890c4db127dc14b
Ankush Sankhe <ankush.sankhe@tripin.co.in>
2018-01-02 19:05:55 +0530

    Added Form 4

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,11 +1,16 @@
-(function($) {
+var count = 1;
+var values2 = {};
+var values3 = {};
+var values4 = {};
+
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (
       location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
@@ -24,7 +29,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function() {
+  $(".js-scroll-trigger").click(function () {
     $(".navbar-collapse").collapse("hide");
   });
 
@@ -35,7 +40,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -72,7 +77,7 @@
     300
   );
 
-  $(window).on("load", function() {
+  $(window).on("load", function () {
     console.log("Ready to rock!!!");
   });
 })(jQuery); // End of use strict
@@ -82,12 +87,12 @@
  * Copyright 2013-2017 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
  */
-!(function(o) {
+!(function (o) {
   "use strict";
-  o('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  o('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (
       location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var t = o(this.hash);
@@ -104,19 +109,19 @@
         );
     }
   }),
-    o(document).scroll(function() {
+    o(document).scroll(function () {
       o(this).scrollTop() > 100
         ? o(".scroll-to-top").fadeIn()
         : o(".scroll-to-top").fadeOut();
     }),
-    o(".js-scroll-trigger").click(function() {
+    o(".js-scroll-trigger").click(function () {
       o(".navbar-collapse").collapse("hide");
     }),
     o("body").scrollspy({
       target: "#mainNav",
       offset: 80
     });
-  var t = function() {
+  var t = function () {
     o("#mainNav").offset().top > 100
       ? o("#mainNav").addClass("navbar-shrink")
       : o("#mainNav").removeClass("navbar-shrink");
@@ -129,31 +134,31 @@
       focus: "#username",
       modal: !0
     }),
-    o(document).on("click", ".portfolio-modal-dismiss", function(t) {
+    o(document).on("click", ".portfolio-modal-dismiss", function (t) {
       t.preventDefault(), o.magnificPopup.close();
     }),
-    o(function() {
+    o(function () {
       o("body")
-        .on("input propertychange", ".floating-label-form-group", function(t) {
+        .on("input propertychange", ".floating-label-form-group", function (t) {
           o(this).toggleClass(
             "floating-label-form-group-with-value",
             !!o(t.target).val()
           );
         })
-        .on("focus", ".floating-label-form-group", function() {
+        .on("focus", ".floating-label-form-group", function () {
           o(this).addClass("floating-label-form-group-with-focus");
         })
-        .on("blur", ".floating-label-form-group", function() {
+        .on("blur", ".floating-label-form-group", function () {
           o(this).removeClass("floating-label-form-group-with-focus");
         });
     });
 })(jQuery);
 
 // When the DOM is ready, run this function
-$(document).ready(function() {
-  static var totalpartcipant = 1;
+$(document).ready(function () {
+  var totalpartcipant = 1;
 
-  $("#numberpart").change(function() {
+  $("#numberpart").change(function () {
     var price = Number($(this).val());
     // console.log(price)
     var total = price * 13500;
@@ -168,13 +173,13 @@ $(document).ready(function() {
 
 
   // updatePrice(1);
-  $("#onlineform1").click(function(e) {
+  $("#onlineform1").click(function (e) {
     var pname = $("#numberpart").val();
     totalpartcipant = pname;
     var batch = $("#batch").val();
     var batch = $('#batch').find(":selected").val();
     // $('#aioConceptName').find(":selected").text();
-    console.log("Batch ==> "+batch);
+    console.log("Batch ==> " + batch);
     // var name = $("#name").val();
     // var email = $("#email").val();
     // var msg = $("#msg").val();
@@ -189,7 +194,7 @@ $(document).ready(function() {
       // document.getElementById("totalprice").placeholder = tp;
       $("#totalprice").attr("placeholder", tp);
       $("#totalprice").attr("value", tp);
-      $.each($("#form1final").serializeArray(), function(i, field) {
+      $.each($("#form1final").serializeArray(), function (i, field) {
         values[field.name] = field.value;
         // valueform1[field.name] = field.value;
       });
@@ -209,7 +214,7 @@ $(document).ready(function() {
 
       form.addEventListener(
         "submit",
-        function(event) {
+        function (event) {
           if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
@@ -217,7 +222,7 @@ $(document).ready(function() {
           form.classList.add("was-validated");
           var values = {};
           // var pname = $("#numberpart").val();
-          $.each($("#form1final").serializeArray(), function(i, field) {
+          $.each($("#form1final").serializeArray(), function (i, field) {
             values[field.name] = field.value;
           });
           // console.log("Here..");
@@ -231,29 +236,29 @@ $(document).ready(function() {
   });
 
 
-  
 
-  
 
-  $("#firstdetailsubmit").click(function(e) {
+
+
+  $("#firstdetailsubmit").click(function (e) {
     console.log("In second form");
     var fname = $("#fname").val();
     var lname = $("#lname").val();
     var email = $("#email").val();
     var validemail = isValidEmailAddress(email);
-    console.log("valdEmail ==> "+ validemail);
+    console.log("valdEmail ==> " + validemail);
     var bloodgroup = $('#bloodgroup').find(":selected").val();
     var meal = $('#meal').find(":selected").val();
     function isValidEmailAddress(emailAddress) {
       var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
       return pattern.test(emailAddress);
-  };
-  
+    };
+
     if (!(fname == "" || lname == "" || validemail == false || bloodgroup == "" || meal == "")) {
       // $("#submitdata").empty();
       e.preventDefault();
-      var values2 = {};
-      $.each($("#form2final").serializeArray(), function(i, field) {
+      values2 = {};
+      $.each($("#form2final").serializeArray(), function (i, field) {
         values2[field.name] = field.value;
         // valueform1[field.name] = field.value;
       });
@@ -262,7 +267,7 @@ $(document).ready(function() {
       console.log("Filled participant data...");
       $("#exampleModalform2").modal("hide");
       $("#exampleModalform3").modal("show");
-      document.getElementById("form3final").reset(); 
+      document.getElementById("form3final").reset();
       $("body").css("overflow", "hidden");
       $("#exampleModalform3").css("overflow", "auto");
 
@@ -274,21 +279,21 @@ $(document).ready(function() {
 
       form.addEventListener(
         "submit",
-        function(event) {
+        function (event) {
           var email = $("#email").val();
           var validemail = isValidEmailAddress(email);
-          console.log("Email submit ==> "+ email);
+          console.log("Email submit ==> " + email);
           if (form.checkValidity() === false || validemail === false) {
             event.preventDefault();
             event.stopPropagation();
-            if(validemail === false) {
+            if (validemail === false) {
               console.log(validemail);
               console.log("Email Invalid..........");
               $("#email").next().empty();
               $("#email").next().text("Email is invalid.");
               $(".emailinvalid").css('display', 'block');
             }
-            else{
+            else {
               console.log(validemail);
               console.log("Email Valid...");
               $(".emailinvalid").css('display', 'none');
@@ -298,7 +303,7 @@ $(document).ready(function() {
           // e.preventDefault();
           var values2 = {};
           // var pname = $("#numberpart").val();
-          $.each($("#form2final").serializeArray(), function(i, field) {
+          $.each($("#form2final").serializeArray(), function (i, field) {
             values2[field.name] = field.value;
           });
           // console.log("Here..");
@@ -312,27 +317,28 @@ $(document).ready(function() {
   });
 
 
-  
-  $("#detailsubmit").click(function(e) {
+
+  $("#detailsubmit").click(function (e) {
     console.log("In third form");
+    console.log(count);
     var relation = $('#relation').find(":selected").val();
     var pfname = $("#pfname").val();
     var plname = $("#plname").val();
     var pemail = $("#pemail").val();
     var validemail = isValidEmailAddress(pemail);
-    console.log("valdEmail ==> "+ validemail);
+    console.log("valdEmail ==> " + validemail);
     var number = $("#contactno1").val();
     var numbertype = $('#contacttype1').find(":selected").val();
     function isValidEmailAddress(emailAddress) {
       var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
       return pattern.test(emailAddress);
-  };
-  
+    };
+
     if (!(pfname == "" || plname == "" || validemail == false || relation == "" || numbertype == "" || number == "")) {
       // $("#submitdata").empty();
       e.preventDefault();
-      var values3 = {};
-      $.each($("#form3final").serializeArray(), function(i, field) {
+      values3 = {};
+      $.each($("#form3final").serializeArray(), function (i, field) {
         values3[field.name] = field.value;
         // valueform1[field.name] = field.value;
       });
@@ -340,32 +346,15 @@ $(document).ready(function() {
       console.log(values3);
       console.log("Filled participant parent data...");
       $("#exampleModalform3").modal("hide");
-      $("#exampleModalthank").modal("show");
+      $("#exampleModalform4").modal("show");
       $("body").css("overflow", "hidden");
-      $("#exampleModalthank").css("overflow", "auto");
-      console.log(i);
+      $("#exampleModalform4").css("overflow", "auto");
+      // console.log(i);
       console.log(totalpartcipant);
 
       //values , values2 , values3
       //Save the data to google form here 
 
-      // count = 1;
-      // while(count <= totalpartcipant){
-      //   console.log(count);
-      //   console.log("Partcipant ==> "+ count);
-      //   $("#finalclose").click(function(e) {
-      //     count++;
-      //     console.log(count);
-      //     console.log("Value of count ==> "+ count);
-      //     e.preventDefault();           
-      //     $("#exampleModalform2").modal("show");
-      //     document.getElementById("form2final").reset();
-      //     $("body").css("overflow", "hidden");
-      //     $("#exampleModalform2").css("overflow", "auto");
-      //   });
-      // }
-
-      // $("#submitdata").append("Name: " + pname + "Values" + values);
     } else {
       // alert("Please Fill All Fields.");
       console.log("Else..");
@@ -375,21 +364,21 @@ $(document).ready(function() {
 
       form.addEventListener(
         "submit",
-        function(event) {
+        function (event) {
           var pemail = $("#pemail").val();
           var validemail = isValidEmailAddress(pemail);
-          console.log("Email submit ==> "+ pemail);
+          console.log("Email submit ==> " + pemail);
           if (form.checkValidity() === false || validemail === false) {
             event.preventDefault();
             event.stopPropagation();
-            if(validemail === false) {
+            if (validemail === false) {
               console.log(validemail);
               console.log("Email Invalid..........");
               $("#pemail").next().empty();
               $("#pemail").next().text("Email is invalid.");
               $(".emailinvalid").css('display', 'block');
             }
-            else{
+            else {
               console.log(validemail);
               console.log("Email Valid...");
               $(".emailinvalid").css('display', 'none');
@@ -400,7 +389,7 @@ $(document).ready(function() {
           console.log("Clcked submitt......");
           var values3 = {};
           // var pname = $("#numberpart").val();
-          $.each($("#form3final").serializeArray(), function(i, field) {
+          $.each($("#form3final").serializeArray(), function (i, field) {
             values3[field.name] = field.value;
           });
           // console.log("Here..");
@@ -412,6 +401,83 @@ $(document).ready(function() {
       );
     }
   });
+
+  $('#onlinepay').click(function (e) {
+
+    //Integrate Pay U code here
+    bolt.launch({
+      key: 'rjQUPktU',
+      txnid: '123456789',
+      hash: 'defdfaadgerhetiwerer',
+      amount: '1',
+      firstname: 'Jaysinh',
+      email: 'dummyemail@dummy.com',
+      phone: '6111111111',
+      productinfo: 'Bag',
+      surl : 'https://sucess-url.in',
+      furl: 'https://fail-url.in'
+      
+      },{ responseHandler: function(response){
+      // your payment response Code goes here
+      },
+      catchException: function(response){
+      // the code you use to handle the integration errors goes here
+      }
+      });
+
+  });
+
+  $("#cheque").click(function (e) {
+    console.log("In Fourth Cheque form");
+    // console.log(count);
+    var erelation = $('#erelation').find(":selected").val();
+    var efname = $("#e1name").val();
+    var enumber = $("#econtactno1").val();
+
+    if (!(e1name == "" || erelation == "" || enumber == "")) {
+      // $("#submitdata").empty();
+      e.preventDefault();
+      values4 = {};
+      $.each($("#form4final").serializeArray(), function (i, field) {
+        values4[field.name] = field.value;
+        // valueform1[field.name] = field.value;
+      });
+      console.log("Here..");
+      console.log(values4);
+      console.log("Filled participant parent data...");
+      $("#exampleModalform4").modal("hide");
+      $("#exampleModalthank").modal("show");
+      $('#messagedetails').text("Payment for "+ values2.FirstName + " , Event Name");
+      $("body").css("overflow", "hidden");
+      $("#exampleModalthank").css("overflow", "auto");
+
+      // $("#submitdata").append("Name: " + pname + "Values" + values);
+    } else {
+      // alert("Please Fill All Fields.");
+      console.log("Else..");
+      var form = document.getElementById("form4final");
+
+      form.addEventListener(
+        "submit",
+        function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add("was-validated");
+          // e.preventDefault();
+          console.log("Clcked submitt......");
+          var values4 = {};
+          // var pname = $("#numberpart").val();
+          $.each($("#form4final").serializeArray(), function (i, field) {
+            values4[field.name] = field.value;
+          });
+        },
+        false
+      );
+    }
+  });
+
 
 
 
@@ -453,7 +519,7 @@ $(document).ready(function() {
 //   $("#exampleModalthank").css("overflow", "auto");
 // });
 
-$(".cancelclass").click(function(e) {
+$(".cancelclass").click(function (e) {
   e.preventDefault();
   $("body").css("overflow", "auto");
 });
@@ -468,7 +534,7 @@ $(".cancelclass").click(function(e) {
 // $('body').css('overflow', 'auto');
 // });
 
-$("#formGroupExampleInput1").keyup(function() {
+$("#formGroupExampleInput1").keyup(function () {
   if ($(this).val() == "") {
     //Check to see if there is any text entered
     // If there is no text within the input ten disable the button
